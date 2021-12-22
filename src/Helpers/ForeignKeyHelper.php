46809b12331af619c08da130c7aba813ef6a1684
llew <llewellyn@zekini.com>
2021-12-22 07:21:26 +0200

    sync

--- src/Helpers/ForeignKeyHelper.php
+++ src/Helpers/ForeignKeyHelper.php
@@ -70,7 +70,7 @@ class ForeignKeyHelper
                 continue;
             }
 
-            if ($value['is_nullable'] !== 'NO' || $value['data_type'] !== 'bigint' || $value['column_type'] !== 'bigint unsigned') {
+            if ($value['is_nullable'] !== 'NO' || $value['data_type'] !== 'bigint' || $value['column_type'] !== 'BIGINT(20) unsigned') {
                 $statement = 'ALTER TABLE ' . $value['table_name'] . ' CHANGE COLUMN ' . $field_name . ' ' . $field_name . ' BIGINT UNSIGNED NOT NULL;';
                 logger(__CLASS__ . ' ' . __FUNCTION__ . ': Fk is wrong (is_nullable:' . $value['is_nullable'] . ' data_type:' . $value['data_type'] . ' column_type:' . $value['column_type'] . '). Recreating: ' . $statement);
                 DB::statement($statement);
