db584fd5d2c2712caf8854f39c087e12945ce2b6
llew <llewellyn@zekini.com>
2022-03-04 09:49:29 +0200

    createAdminUser

--- src/Traits/CreateTestUser.php
+++ src/Traits/CreateTestUser.php
@@ -8,9 +8,17 @@ use App\Models\User;
 
 trait CreateTestUser
 {
-    public function createUser(?array $array = [])
+    public function createUser(?array $array = []): User
     {
         $user = User::factory()->create($array);
+
+        return User::findOrFail($user->id);
+    }
+
+    public function createAdminUser(?array $array = []): User
+    {
+        $user = User::factory()->create($array);
+        $user->assignRole('super-admin');
 
         return User::findOrFail($user->id);
     }
